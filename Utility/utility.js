a94150bc946dd652c096a676cb6b5b9633a18d53
Admin <prashanth.murthy007@gmail.com>
2019-01-23 19:10:01 +0530

    first commit

--- Utility/utility.js
+++ Utility/utility.js
@@ -7,12 +7,24 @@ module.exports = {
         {
         console.log(output);
         }
-        else{
+        else
+        {
             console.log("name should contains  3 or more character and should not begin with number");
-        }
-       
+        }   
     },
 
+
+    LeapYear(year)
+    {
+        if(year%100==0 || year%400==0 || year%4==0)
+        {
+            console.log("is leap year");
+        }
+        else
+        {
+            console.log("not a leap year");
+        }
+    }
    
 
 
